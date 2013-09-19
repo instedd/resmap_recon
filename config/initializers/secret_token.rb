8a3ed7eaf263c44068914c1c7313c091d40de536
Brian J. Cardiff <bcardiff@manas.com.ar>
2013-09-19 11:13:15 -0300

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ResmapRecon::Application.config.secret_token = '6cd1ee221a6b4ed9f47e9a51541991602049a4eaa8c4b859c0b6420b61fb526fc43f9919c2b78f808165c88accfc69db792d7d5293fc231619e54b85e4043593'
