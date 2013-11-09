0fd1d3fac6608b9843b10021ae61d942a0a71fac
Brian J. Cardiff <bcardiff@manas.com.ar>
2013-11-08 23:32:13 -0300

    fix heroku deployment

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,8 @@ module ResmapRecon
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.initialize_on_precompile = false
+
     config.generators do |g|
       g.test_framework :rspec
       g.template_engine :haml
