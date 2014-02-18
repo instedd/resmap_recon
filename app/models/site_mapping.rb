5e7c62941f5faaea1e06f2e8084e8fbd3d88210c
Ary Borenszweig <aborenszweig@manas.com.ar>
2014-02-18 14:24:36 -0300

    Fixed not_curated scope

--- app/models/site_mapping.rb
+++ app/models/site_mapping.rb
@@ -6,6 +6,6 @@ class SiteMapping < ActiveRecord::Base
   validates_presence_of :source_list_id, :name
 
   scope :pending, where('mfl_hierarchy IS NULL AND dismissed = FALSE')
-  scope :not_curated, where('mfl_hierarchy IS NOT NULL AND dismissed = FALSE')
+  scope :not_curated, where('mfl_hierarchy IS NOT NULL AND mfl_site_id IS NULL AND dismissed = FALSE')
   scope :curated, where('mfl_site_id IS NOT NULL')
 end
