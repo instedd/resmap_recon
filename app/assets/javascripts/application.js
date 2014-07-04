dafc32308c4892970e7ad060e856a2fe4bdd6d67
Juan Wajnerman <jwajnerman@manas.com.ar>
2014-07-04 18:05:28 -0300

    Show administrative division field in the merge view (fixes #77)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,7 +22,7 @@
 
 angular.module('ProjectSourceAfterCreateApp', ['Rails','ProjectSourceAfterCreate', 'Ladda']);
 angular.module('MappingEditorApp', ['Rails','MappingEditor', 'HierarchyViewer', 'RmApiDirectives']);
-angular.module('CurationApp', ['Rails','DndValue','Curation', 'HierarchyViewer', 'LocationInput', 'Ladda', 'PendingList', 'RmApiDirectives', 'PaginatedGrid', 'SourceListCombo']);
+angular.module('CurationApp', ['Rails','DndValue','Curation', 'HierarchyViewer', 'LocationInput', 'Ladda', 'PendingList', 'RmApiDirectives', 'PaginatedGrid', 'SourceListCombo', 'HierarchyInput']);
 angular.module('MasterSitesEditorApp', ['Rails','DndValue','MasterSitesEditor', 'HierarchyViewer', 'LocationInput', 'RmApiDirectives', 'HierarchyInput']);
 angular.module('HierarchySelectionApp', ['Rails','HierarchySelection']);
 angular.module('FacilityPromotionApp', ['Rails', 'FacilityPromotion', 'RmApiDirectives']);
